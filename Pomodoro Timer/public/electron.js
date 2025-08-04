aabb42f2ff0f0aa860674934a1e837bff80907cc
aliciachickenwings <aachalshresth@gmail.com>
2025-08-04 14:55:15 +0200

    Changed app icon

--- Pomodoro Timer/public/electron.js
+++ Pomodoro Timer/public/electron.js
@@ -12,6 +12,7 @@ function createWindow() {
     titleBarStyle: 'hidden',
     titleBarOverlay: false,
     alwaysOnTop: true,
+    icon: path.join(__dirname, 'app.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
